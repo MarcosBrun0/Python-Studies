3c7df672f53b7d04d51e988c609d0d68e65a5eaa
marcosbrun0 <Mbrunocampos20@gmail.com>
2024-02-06 00:20:38 -0300

    day35

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
 
+
 weather_api_key = "9528fcaf7e9e99c0c83dd3fb3382761a"
+email = "marcosbrunocampos2@gmail.com"
+password = "jhrbtpgcujiqklcr"
--- day35-API-Auth/main.py
+++ day35-API-Auth/main.py
@@ -1,11 +1,33 @@
-from config import weather_api_key
-import requests
+from config import weather_api_key,email,password
+import requests, smtplib
 
 lat ="-21.754530"
 lon ="-41.324612"
-url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&cont=3&appid={weather_api_key}"
+cont = 3
+url = f"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&cnt={cont}&appid={weather_api_key}"
 
 response = requests.get(url)
 response.raise_for_status()
 data = response.json()
-print(data["weather"][0]["description"])
+id_code=[]
+for time in range(0,3):
+     id_code.append(data["list"][time]["weather"][0]["id"])
+
+feels_like_data = data["list"][0]["main"]["feels_like"]
+descriptiom = data["list"][0]["weather"][0]["description"]
+
+Celsius_feelslike = int(feels_like_data) -273.15
+will_rain = False
+
+
+for id in id_code:
+    if int(id)<700:
+        will_rain = True
+
+
+if will_rain:
+
+    with smtplib.SMTP("smtp.gmail.com") as conection:
+        conection.starttls()
+        conection.login(user=email,password=password)
+        conection.sendmail(from_addr=email,to_addrs=mbrunocampos20@gmail.com, msg=f"Subject:Its gonna Rain today\n\n will be {descriptiom}, with the temperature feels of {feels_like_data} ")
