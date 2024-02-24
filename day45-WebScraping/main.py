ed48d2956a04881fcb569d91aca73f84f9446467
marcosbrun0 <Mbrunocampos20@gmail.com>
2024-02-23 21:40:41 -0300

    day45web

--- day45-WebScraping/main.py
+++ day45-WebScraping/main.py
@@ -1,3 +1,11 @@
-from Bs4 import Beautifulsoup
+import requests
+import bs4
 
 
+response = requests.get("https://news.ycombinator.com/")
+web_page =response.text
+soup =bs4.BeautifulSoup(web_page,"html.parser")
+
+var = soup.find(name="a",class_ = "storylink")
+
+print(a)
