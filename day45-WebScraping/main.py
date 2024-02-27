5d709dad486123ccbeff1394e6f09a80d770e062
marcosbrun0 <Mbrunocampos20@gmail.com>
2024-02-26 23:44:50 -0300

    day45web1

--- day45-WebScraping/main.py
+++ day45-WebScraping/main.py
@@ -6,6 +6,15 @@ response = requests.get("https://news.ycombinator.com/")
 web_page =response.text
 soup =bs4.BeautifulSoup(web_page,"html.parser")
 
-var = soup.find(name="a",class_ = "storylink")
+articles = soup.find_all(name="a",class_ = "storylink")
+articles_text = []
+articles_links =[]
+for article_tag in articles:
+    article_text = article_tag.getText()
+    articles_text.append(article_text)
+    article_link = article_tag.get("href")
+    articles_links.append(article_text)
+    article_upvote = soup.find_all(name="span", class_= "score").getText()
 
-print(a)
+
+print(article_tag,article_upvote,article_link,article_text)
