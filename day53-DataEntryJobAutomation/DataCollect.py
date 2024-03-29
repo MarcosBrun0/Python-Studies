0f45282eee4ad3e6e2e54eb5079fcccc1cb0ac15
MarcosBrun0 <mbrunocampos20@gmail.com>
2024-03-29 16:06:13 -0300

    dataentryfinished

--- day53-DataEntryJobAutomation/DataCollect.py
+++ day53-DataEntryJobAutomation/DataCollect.py
@@ -1,5 +1,4 @@
 import re
-
 from bs4 import BeautifulSoup
 import requests
 from pprint import pprint
@@ -7,13 +6,35 @@ response = requests.get(url="https://appbrewery.github.io/Zillow-Clone/")
 website =response.text
 
 soup = BeautifulSoup(website,"html.parser")
+
+list_element = soup.find_all(name="li", class_="ListItem-c11n-8-84-3-StyledListCardWrapper")
+
+#lIST OF LINKS
+
 link_list = []
-list_element = soup.find_all(href=re.compile("zillow"))
-pprint(list_element)
-# for element in list_element:
-#     text = element.text
-#     newsoup = BeautifulSoup(text,"html.parser")
-#     link = newsoup.find(name="a")
-#     link_list.append(link)
-#
-# print(link_list)
+for element in list_element:
+     link = element.find("a")
+     link_list.append(link.get("href"))
+
+#LIST OF ADDRESS
+
+address_list = []
+for element in list_element:
+    address = element.find(name="address")
+    address = [item.strip() for item in address if str(item)]
+    address = address[0]
+    address_list.append(address)
+
+#LIST OF PRICES
+
+price_list = []
+for element in list_element:
+    price = element.find(name="span", class_="PropertyCardWrapper__StyledPriceLine")
+    pricestring= str(price.getText())
+    s = pricestring
+    s = re.sub(r'[\$\+,/]', '', s)
+    # Extract numeric part
+    num = re.findall(r'\d+', s)
+    price_list.append(str(num[0]))
+
+
--- day53-DataEntryJobAutomation/main.py
+++ day53-DataEntryJobAutomation/main.py
@@ -2,8 +2,36 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time, Config
+from DataCollect import link_list,address_list,price_list
+
+
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach",True)
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://forms.gle/s8JqSsg6XU4VkWLn7")
 
 
+adressXpath = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input'
+priceXpath ='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input'
+linkXpath ='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input'
+
+
+for n in range(len(price_list)):
+    time.sleep(0.4)
+    address = driver.find_element(By.XPATH,value=adressXpath)
+    price = driver.find_element(By.XPATH,value=priceXpath)
+    link = driver.find_element(By.XPATH,value=linkXpath)
+    sendbutton = driver.find_element(By.XPATH,value='//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span')
+
+    address.send_keys(address_list[n])
+    price.send_keys(price_list[n])
+    link.send_keys(link_list[n])
+    time.sleep(0.5)
+    sendbutton.click()
+    time.sleep(0.4)
+    newentry = driver.find_element(By.XPATH, value='/html/body/div[1]/div[2]/div[1]/div/div[4]/a')
+    newentry.click()
+    time.sleep(0.4)
+
+driver.quit()
 
